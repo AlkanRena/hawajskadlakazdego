d1c5c29923461f364d28d8f3f38f7ec97d1173db
Dawid <dawid.kubic@gmail.com>
2018-04-14 16:00:43 +0200

    under work

--- app/src/main/java/hackerspace/hawajskadlakazdego/Add.kt
+++ app/src/main/java/hackerspace/hawajskadlakazdego/Add.kt
@@ -1,5 +1,13 @@
 package hackerspace.hawajskadlakazdego
 
+import android.app.*
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
@@ -7,12 +15,56 @@ import kotlinx.android.synthetic.main.activity_add.*
 
 class Add : AppCompatActivity() {
 
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private var channelId = "hackerspace.hawajskadlakazdego"
+    private val description = "Test notification"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
 
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val navigationNotification = R.id.btn_navigation_notifications
+        navigationNotification.setOnClickListener {
+
+            val intent = Intent(this, LauncherActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+                notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+
+                builder = Notification.Builder(this, channelId)
+                        .setContentTitle("Msg me")
+                        .setContentText("Test notify")
+                        .setContentIntent(pendingIntent)
+            } else {
+
+                builder = Notification.Builder(this)
+                        .setContentTitle("Msg me")
+                        .setContentText("Test notify")
+                        .setContentIntent(pendingIntent)
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                notificationManager.notify(1234, builder.build())
+            }
+
+        }
 
     }
 }
+
+private fun Int.setOnClickListener(function: () -> Unit) {
+
+}
+
+
