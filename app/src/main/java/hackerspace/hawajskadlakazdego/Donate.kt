0d051d26b8e40fa650d494a02972b0658d1f4cf5
Dawid Kubić <dawid.kubic@gmail.com>
2018-04-15 13:31:03 +0200

    added donate

---
+++ app/src/main/java/hackerspace/hawajskadlakazdego/Donate.kt
@@ -0,0 +1,12 @@
+package hackerspace.hawajskadlakazdego
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Donate : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donate)
+    }
+}
